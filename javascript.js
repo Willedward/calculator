93dc33f3348447a4fab4680710aa0f5cabedf48d
William Edward Sugiharto <willsugiharto@gmail.com>
2026-01-15 13:37:05 +0800

    there is a bug in the logic of the game

--- javascript.js
+++ javascript.js
@@ -40,49 +40,98 @@ let num2 = "";
 let operator = "";
 let operator_clicked = false;
 let answer = 0;
-num_buttons = document.querySelectorAll('.numbuttons');
+num_buttons = document.querySelectorAll('.numbutton');
+let reseted = false;
 num_buttons.forEach((num_button) =>{
     num_button.addEventListener('click', ()=>{
+        console.log("checker");
+        
+        if(equal_sign && reseted == false){
+                reset();
+                reseted = true;
+        }
         if(operator_clicked==false){
-        num1 += num_button.value;}
+
+            num1 += num_button.textContent;
+            console.log('num1');
+            console.log(num1);
+            display(num1);
+        }
         else if(operator_clicked)
         {
-            num2 += num_button.value;
+            
+            num2 += num_button.textContent;
+            console.log('num2')
+            console.log(num2);
+            display(num2);
         }
     });
 });
 
-operator_buttons = document.querySelectorAll('.operatorbuttons');
-operator_buttons.forEach((operator_button) => {
-    operator_button.addEventListener('click', () =>
-    {
-        operator = operator_button.value;
-    });
-});
-
-//reseting
 const screen = document.querySelector('#screen');
 const ans_display = document.createElement('p');
 ans_display.id = 'ans_display';
 ans_display.textContent = answer;
+console.log(ans_display.textContent);
 screen.appendChild(ans_display);
 
-function display(answer)
+function display(ans)
 {
-    screen.removeChild(ans_display);
+    const curr_display = document.querySelector('#ans_display');
+    screen.removeChild(curr_display);
     const ans_display = document.createElement('p');
     ans_display.id = 'ans_display';
-    ans_display.textContent = answer;
+    ans_display.textContent = ans;
     screen.appendChild(ans_display);
 }
+let first_operator=true;
+let prev_operator = "";
+let equal_sign = false;
+operator_buttons = document.querySelectorAll('.operatorbuttons');
+operator_buttons.forEach((operator_button) => {
+    operator_button.addEventListener('click', () =>
+    {
+        operator = operator_button.textContent;
+        console.log('operator clicked');
+        console.log(operator);
+        if(first_operator == false)
+        {
+            if(equal_sign) //equal_sign signifies state of still just =
+            {
+                prev_operator = operator;
+                equal_sign = false;
+            }
+            else{
+                answer = operate(prev_operator, num1, num2);
+                reseted = false;
+                display(answer);
+            }
+            if(operator === '=') {equal_sign = true; prev_operator = "";}
+            else {prev_operator = operator;}
+            num1 = answer;
+            num2 = "";
+            
+        }else{
+            first_operator = false;
+            operator_clicked = true;
+            prev_operator = operator;
+        }
+    });
+});
+
+//reseting
+
 
 function reset()
 {
-    ans = 0;
+    answer = 5;
     num1 = "";
     num2 = "";
+    first_operator = true;
+    operator_clicked = false;
+    display(answer);
 }
-const ac_button = document.querySelector('.ac');
-ac_button.addEventListener('click', reset);
+const ac_buttons = document.querySelector('#ac');
+ac_buttons.addEventListener('click', reset);
 
 //deciding when to evaluate
